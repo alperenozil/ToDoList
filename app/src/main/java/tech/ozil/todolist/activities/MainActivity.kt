57723a86bd9f65894a6e2ea82f89ba4c619631d3
alperenozil <necdet.ozil@hacettepe.edu.tr>
2021-02-26 20:07:41 +0300

    fragment name shows up in the title

--- app/src/main/java/tech/ozil/todolist/activities/MainActivity.kt
+++ app/src/main/java/tech/ozil/todolist/activities/MainActivity.kt
@@ -2,11 +2,19 @@ package tech.ozil.todolist.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import tech.ozil.todolist.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupActionBarWithNavController(findNavController(R.id.navHostFragment))
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.navHostFragment)
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
