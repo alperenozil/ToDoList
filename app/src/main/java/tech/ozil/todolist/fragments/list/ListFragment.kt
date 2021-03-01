ba6c4e4dd346f10b339b590e4061505adc8d0230
alperenozil <necdet.ozil@hacettepe.edu.tr>
2021-03-01 22:10:21 +0300

    navigating to update fragment from list fragment is implemented.

--- app/src/main/java/tech/ozil/todolist/fragments/list/ListFragment.kt
+++ app/src/main/java/tech/ozil/todolist/fragments/list/ListFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import tech.ozil.todolist.R
@@ -17,6 +18,9 @@ class listFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_list, container, false)
         view.findViewById<FloatingActionButton>(R.id.floatingActionButton).setOnClickListener{
             findNavController().navigate(R.id.action_listFragment_to_addFragment2)
+        }
+        view.findViewById<ConstraintLayout>(R.id.listLayout).setOnClickListener{
+            findNavController().navigate(R.id.action_listFragment_to_updateFragment2)
         }
         return view;
     }
