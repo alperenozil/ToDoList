e3eff5721b8c1aa8b833a00b8f2e1d04e948e393
alperenozil <necdet.ozil@hacettepe.edu.tr>
2021-03-02 20:02:51 +0300

    search bar is added.

--- app/src/main/java/tech/ozil/todolist/fragments/list/ListFragment.kt
+++ app/src/main/java/tech/ozil/todolist/fragments/list/ListFragment.kt
@@ -1,10 +1,8 @@
 package tech.ozil.todolist.fragments.list
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -22,6 +20,11 @@ class listFragment : Fragment() {
         view.findViewById<ConstraintLayout>(R.id.listLayout).setOnClickListener{
             findNavController().navigate(R.id.action_listFragment_to_updateFragment2)
         }
+        setHasOptionsMenu(true)
         return view;
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.list_fragment_menu,menu);
     }
 }
